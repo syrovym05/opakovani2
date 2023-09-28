91edab5c81585f4a93248988f1181fa630b08f54
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-09-28 21:27:12 +0200

    Add files via upload

---
+++ opakovani2/opakovani2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace opakovani2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(30, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Zadej n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(33, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 303);
+            this.listBox1.TabIndex = 18;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(172, 91);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(122, 303);
+            this.listBox2.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 418);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(261, 20);
+            this.textBox2.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(340, 543);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ opakovani2/opakovani2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace opakovani2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.ShowIcon = false;
+            this.Text = "Ukol 2";
+        }
+        int[] pole;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            try
+            {
+                Random rng = new Random();
+                int n = int.Parse(textBox1.Text);
+                pole = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    pole[i] = rng.Next(1,26);                    
+                }
+
+               
+                Vypis(listBox1, pole);
+                
+                int pozice = 0;
+                string s;
+
+                Array.Reverse(pole);
+                //int prvocislo = PoslPrvocilo(ref pozice);
+                // MessageBox.Show("Posledni prvocislo je: " + prvocislo + " na indexu: " + pozice);
+                _ = PoslPrvocilo(ref pozice) == 0 ? s = "V poli se nenachazi zadne prvocislo" : s = String.Format("Posledni prvnocislo je {0} a je na indexu {1}", PoslPrvocilo(ref pozice),pozice);
+                Array.Reverse(pole);
+
+                MessageBox.Show(s);
+                Vymen();
+
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        void Vypis(ListBox listbox, int[] pole)
+        {
+            listbox.Items.Clear();
+            foreach (int i in pole)
+            {
+                listbox.Items.Add(i);
+            }
+        }
+
+        void Vymen()
+        {
+            int prvni = pole[0];
+            int posledni = pole[pole.Length-1];
+            pole[0] = posledni;
+            pole[pole.Length - 1] = prvni;
+            Vypis(listBox2, pole);
+        }
+
+        int PoslPrvocilo(ref int pozice)
+        {
+            
+            for (int i = 0; i < pole.Length;i++)
+            {
+                bool skip = false;
+                pozice = pole.Length-1-i;
+                int cislo = pole[i];
+                if (cislo == 2) return cislo;
+                else if (cislo % 2 == 0 || cislo == 1) continue;
+                else
+                {
+                    for (int j = 3; j <= (cislo)/2; j += 2)
+                    {
+                        if (cislo % j == 0)
+                        {                            
+                            skip = true;
+                            break;
+                        }
+                    }
+
+                }
+                if (skip) continue;
+                return cislo;                 
+            }
+            return 0;
+            
+        }
+
+        string Maximalni(string s)
+        {
+            while (s.Contains("  ")) s = s.Replace("  ", " ");
+            if (s == " " || s== "") return "Špatný vstup";
+            string[] pole = s.Split(' ');
+
+            return "Nejdelsi prvek: "+ pole.OrderByDescending(x => x.Length).First();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+             MessageBox.Show(Maximalni(textBox2.Text));
+        
+        }
+    }
+}
